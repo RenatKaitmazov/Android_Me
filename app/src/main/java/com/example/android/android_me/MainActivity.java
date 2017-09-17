c439b9a23451ecba115d1d855e18458fc967ceec
Renat Kaitmazov <renatkaitmazov@gmail.com>
2017-09-17 20:48:18 +0300

    Update build tool version

--- app/src/main/java/com/example/android/android_me/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/MainActivity.java
@@ -3,10 +3,10 @@ package com.example.android.android_me;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
