7e806958fdd2f25c4ee6d4bda1def7e58b57faa1
Renat Kaitmazov <renatkaitmazov@gmail.com>
2017-09-17 21:57:23 +0300

    Add three fragments to the main activity

--- app/src/main/java/com/example/android/android_me/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/BodyPartFragment.java
@@ -1,9 +1,8 @@
 package com.example.android.android_me;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,6 +11,9 @@ import android.view.ViewGroup;
 
 import com.example.android.android_me.databinding.FragmentBodyPartBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Renat Kaitmazov
  */
@@ -19,10 +21,18 @@ import com.example.android.android_me.databinding.FragmentBodyPartBinding;
 public final class BodyPartFragment extends Fragment {
 
     /*------------------------------------------------------------------------*/
+    // Static
+    /*------------------------------------------------------------------------*/
+
+    private static final String ARGUMENT_IMAGES = "ARGUMENT_IMAGES";
+
+    /*------------------------------------------------------------------------*/
     // Fields
     /*------------------------------------------------------------------------*/
 
     private FragmentBodyPartBinding binding;
+    private List<Integer> imageIds;
+    private int currentImageId;
 
     /*------------------------------------------------------------------------*/
     // Constructors
@@ -35,24 +45,40 @@ public final class BodyPartFragment extends Fragment {
     // Lifecycle Events
     /*------------------------------------------------------------------------*/
 
+    @Override
+    public final void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+        final Bundle args = getArguments();
+        if (args != null) {
+            imageIds = args.getIntegerArrayList(ARGUMENT_IMAGES);
+        }
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_body_part, container, false);
-        setStartingImage();
+        if (imageIds != null) {
+            @DrawableRes
+            final Integer imageResId = imageIds.get(0);
+            binding.bodyPartImageView.setImageResource(imageResId);
+        }
         return binding.getRoot();
     }
 
     /*------------------------------------------------------------------------*/
-    // Helper Methods
+    // API
     /*------------------------------------------------------------------------*/
 
-    // Fake image for the image view just to observe that the fragment is there.
-    private void setStartingImage() {
-        final int greenColor = 0x00FF00;
-        final Drawable greenColorDrawable = new ColorDrawable(greenColor);
-        binding.bodyPartImageView.setImageDrawable(greenColorDrawable);
+    public static BodyPartFragment newInstance(ArrayList<Integer> imageIds) {
+        final Bundle args = new Bundle(1);
+        args.putIntegerArrayList(ARGUMENT_IMAGES, imageIds);
+        final BodyPartFragment fragment = new BodyPartFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 }
--- app/src/main/java/com/example/android/android_me/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.android.android_me;
 
+import android.databinding.DataBindingUtil;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +16,33 @@ public final class MainActivity extends AppCompatActivity {
     @Override
     protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        final BodyPartFragment headFragment = BodyPartFragment.newInstance(null);
+        final BodyPartFragment bodyFragment = BodyPartFragment.newInstance(null);
+        final BodyPartFragment legsFragment = BodyPartFragment.newInstance(null);
+
+        addFragment(R.id.head_container, headFragment);
+        addFragment(R.id.body_container, bodyFragment);
+        addFragment(R.id.legs_container, legsFragment);
+    }
+
+    /*------------------------------------------------------------------------*/
+    // API
+    /*------------------------------------------------------------------------*/
+
+    /*------------------------------------------------------------------------*/
+    // Helper Methods
+    /*------------------------------------------------------------------------*/
+
+    private void addFragment(@IdRes int fragmentContainerId, Fragment fragment) {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment currentFragment = fragmentManager.findFragmentById(fragmentContainerId);
+        if (currentFragment == null) {
+            // The container is empty. Add the specified fragment to it.
+            fragmentManager.beginTransaction()
+                    .add(fragmentContainerId, fragment)
+                    .commitNow();
+        }
     }
 }
